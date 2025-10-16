d265dc5f40e9b740d57398cdfd9b40ecd99818fa
siddiquimoid <siddiquimoid@eng.rizvi.edu.in>
2025-10-17 00:24:11 +0530

    Add files via upload

---
+++ exp1-task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char name[50] = "Moid";
+    int age = 19;
+    char class[50] = "Comps";
+    char divi = 'C';
+    int uin = 251;
+    int uin2 = 06;
+
+    printf("My name is %s", name);
+    printf("\nMy age is %i", age);
+    printf("\nMy class is %s", class);
+    printf("\nMy divition is %c", divi);
+    printf("\nMy name is %iP%i9", uin, uin2);
+}
